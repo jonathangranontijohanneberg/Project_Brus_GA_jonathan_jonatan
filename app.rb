d5667d6e6ee6f260759990b9f159601a5775b43a
jonathangranontijohanneberg <93259651+jonathangranontijohanneberg@users.noreply.github.com>
2022-09-29 16:06:52 +0200

    started filling in with code from previous projects

--- app.rb
+++ app.rb
@@ -5,8 +5,35 @@ require 'slim'
 # get('/') do
 
 # end
+class NoiseClass
+    def initialize(noise_1, noise_2)
+        @noise_1 = noise_1
+        @noise_2 = noise_2
+    end
+
+    def noises
+        x = NoiseClass.new
+        
+        {noise_1: @noise_1, noise_2: @noise_2}
+    end
+
+end
+
+
 get('/home_screen') do
-    
+    noises =
+    @noise_info = [ ,
+        {
+        # sound1: "Snobben",
+        # sound2: "329"
+        }
+    ]
     slim(:home_screen)
 
 end
+
+# section id="sound_section"
+#     - @sound_info.each do |info|
+#         section style="background: bisque" class="color_sections"
+#             section style="width: 100px; height: 100px;" class="sound_sections" = info[:noise_1]
+#             section style="width: 100px; height: 100px;" class="sound_sections" = info[:noise_2]
