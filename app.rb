7714db38cc05bee986d4b3ca6db3cd1ebe321544
jonathangranontijohanneberg <93259651+jonathangranontijohanneberg@users.noreply.github.com>
2022-09-29 18:04:21 +0200

    added code and moved code, added txt-file

--- app.rb
+++ app.rb
@@ -5,35 +5,38 @@ require 'slim'
 # get('/') do
 
 # end
-class NoiseClass
-    def initialize(noise_1, noise_2)
-        @noise_1 = noise_1
-        @noise_2 = noise_2
-    end
 
-    def noises
-        x = NoiseClass.new
-        
-        {noise_1: @noise_1, noise_2: @noise_2}
-    end
-
-end
 
 
 get('/home_screen') do
-    noises =
-    @noise_info = [ ,
+    # noises =
+    @noise_info = [
         {
-        # sound1: "Snobben",
-        # sound2: "329"
+            noise_name: "White Noise",
+            noise_background_img: "white_noise_img.jpg"
+            # noise_file: ""
+            # /img/sinatra_img.jpg
+        },
+        {
+            noise_name: "Red Noise",
+            noise_background_img: "brown_noise_img.jpg"
+        },
+        {
+            noise_name: "Pink Noise",
+            noise_background_img: "pink_noise_img.jpg"
+
+        },
+        {
+            noise_name: "Brown Noise",
+            noise_background_img: "brown_noise_img.jpg"
         }
     ]
     slim(:home_screen)
 
 end
 
-# section id="sound_section"
-#     - @sound_info.each do |info|
+# section id="noise_section"
+#     - @noise_info.each do |info|
 #         section style="background: bisque" class="color_sections"
-#             section style="width: 100px; height: 100px;" class="sound_sections" = info[:noise_1]
-#             section style="width: 100px; height: 100px;" class="sound_sections" = info[:noise_2]
+#             section style="width: 100px; height: 100px;" class="noise_sections" = info[:noise_1]
+#             section style="width: 100px; height: 100px;" class="noise_sections" = info[:noise_2]
