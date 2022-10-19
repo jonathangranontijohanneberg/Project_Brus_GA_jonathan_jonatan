25a78313f0c7bd051046499fba51a10703ca12d6
jonathangranontijohanneberg <93259651+jonathangranontijohanneberg@users.noreply.github.com>
2022-10-19 14:55:28 +0200

    made it so that the "pop-up" windows do not stack, only one can exist at a time, when activating one after the other.

--- public/js/script.js
+++ public/js/script.js
@@ -13,21 +13,29 @@ contact.style.visibility = visibility_contact;
 function toggleMenu() {
     if (visibility_about_us == 'hidden') {
         visibility_about_us = 'visible';
+        visibility_contact = 'hidden';
+
     }
     else {
         visibility_about_us = 'hidden';
+        visibility_contact = 'hidden';
     }
     about_us.style.visibility = visibility_about_us;
+    contact.style.visibility = visibility_contact;
     about_us_link.classList.toggle("about_us_link")
 }
 
 function toggleMenu2() {
     if (visibility_contact == 'hidden') {
         visibility_contact = 'visible';
+        visibility_about_us = 'hidden';
+
     }
     else {
         visibility_contact = 'hidden';
+        visibility_about_us = 'hidden';
     }
+    about_us.style.visibility = visibility_about_us;
     contact.style.visibility = visibility_contact;
     contact_link.classList.toggle("contact_link")
 }
